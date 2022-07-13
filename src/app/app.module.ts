2792c14e96ee1b1a1cb9591df46ada8b533cf440
Nina Hovinmaa <nina.hovinmaa@enfogroup.com>
2022-07-13 15:34:49 +0200

    refactoring, break out the details of the dish to a separate component. Add comments.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
 import 'hammerjs';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
