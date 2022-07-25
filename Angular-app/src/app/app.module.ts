268c001cdeaf6f79b615c3a6fc2783666a1bc33d
Nina Hovinmaa <nina.hovinmaa@enfogroup.com>
2022-07-25 17:08:19 +0200

    add feedback form for individual dishes

--- Angular-app/src/app/app.module.ts
+++ Angular-app/src/app/app.module.ts
@@ -1,12 +1,17 @@
+//general imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import 'hammerjs';
+
+//angular material
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
-import { MenuComponent } from './menu/menu.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -14,13 +19,14 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { HttpClientModule } from '@angular/common/http';
-import 'hammerjs';
+import { MatSliderModule } from '@angular/material/slider';
+
+//internal components
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -29,6 +35,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 
+//services
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
@@ -63,7 +70,8 @@ import { LeaderService } from './services/leader.service';
     MatSelectModule,
     MatSlideToggleModule,
     MatProgressSpinnerModule,
-    HttpClientModule
+    HttpClientModule,
+    MatSliderModule
   ],
   providers: [
     DishService, 
--- Angular-app/src/app/dishdetail/dishdetail.component.ts
+++ Angular-app/src/app/dishdetail/dishdetail.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../shared/comment';
 
 
 @Component({
@@ -17,15 +19,41 @@ export class DishdetailComponent implements OnInit {
   dishIds: string[];
   prev: string;
   next: string;
+  commentForm: FormGroup;
+  comment: Comment;
+  @ViewChild('cform') commentFormDirective: any;
+
+  formErrors: any = {
+    rating: '',
+    comment: '',
+    author: ''
+  }
+
+  validationMessages: any = {
+    'comment': {
+      'required': 'Comment is required'
+    },
+    'author': {
+      'required': 'Name is required',
+      'minlength': 'Name must be at least 2 characters long'
+    }
+  }
 
   constructor(private dishService: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder,
+    ) { }
 
   ngOnInit(): void {
     this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-    this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+    this.route.params
+      .pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
+      .subscribe(dish => { 
+        this.dish = dish; 
+        this.setPrevNext(dish.id); 
+      });
+      this.createForm();
   }
 
   setPrevNext(dishId: string) {
@@ -37,5 +65,50 @@ export class DishdetailComponent implements OnInit {
   goBack() {
     this.location.back();
   }
+
+  createForm() {
+    this.commentForm = this.fb.group({
+      rating: 5,
+      comment: ['', Validators.required],
+      author: ['', [Validators.required, Validators.minLength(2)]],
+    })
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data))
+  }
+
+  onValueChanged(data?: any){
+    if (!this.commentForm) {
+      return
+    }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        //clear previous error message (if any)
+        this.formErrors[field]='';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
+  onSubmit() {
+    this.comment = this.commentForm.value;
+    //set form back to pristine
+    this.commentFormDirective.resetForm();
+    //reset values to default
+    this.commentForm.reset({
+      rating: 5,
+      comment: '',
+      author: '',
+    })
+  }
+  
 
 }
