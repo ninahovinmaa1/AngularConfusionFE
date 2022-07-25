51ecb0348d548b0d0d11fc446c2ba1cd101af19a
Nina Hovinmaa <nina.hovinmaa@enfogroup.com>
2022-07-25 17:30:07 +0200

    use data from server with http client

--- Angular-app/src/app/app.module.ts
+++ Angular-app/src/app/app.module.ts
@@ -35,10 +35,13 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 
-//services
+//internal services
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
+
+//other internal
+import { baseURL } from './shared/baseurl';
 @NgModule({
   declarations: [
     AppComponent,
@@ -77,6 +80,7 @@ import { LeaderService } from './services/leader.service';
     DishService, 
     PromotionService, 
     LeaderService,
+    { provide: 'BaseURL', useValue: baseURL }
   ],
   entryComponents: [LoginComponent],
   bootstrap: [AppComponent]
