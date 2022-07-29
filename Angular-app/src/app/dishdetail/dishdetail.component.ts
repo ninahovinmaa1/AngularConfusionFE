265356b63864d9850723426831b1f113a35eb8dc
Nina Hovinmaa <nina.hovinmaa@enfogroup.com>
2022-07-29 10:12:22 +0200

    fix images of home and dishdetails

--- Angular-app/src/app/dishdetail/dishdetail.component.ts
+++ Angular-app/src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, Inject } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
@@ -39,11 +39,13 @@ export class DishdetailComponent implements OnInit {
     }
   }
 
-  constructor(private dishService: DishService,
+  constructor(
+    private dishService: DishService,
     private route: ActivatedRoute,
     private location: Location,
     private fb: FormBuilder,
-    ) { }
+    @Inject('BaseURL') public BaseURL: any
+  ) { }
 
   ngOnInit(): void {
     this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
