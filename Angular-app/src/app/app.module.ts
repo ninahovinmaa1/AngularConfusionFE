5a0cc785adb8b382d76b8d5a4315941bc661d8d4
Nina Hovinmaa <nina.hovinmaa@epicalgroup.com>
2023-09-19 11:29:20 +0200

    add directive to highlight menu items on hover

--- Angular-app/src/app/app.module.ts
+++ Angular-app/src/app/app.module.ts
@@ -43,6 +43,7 @@ import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 
 //other internal
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -53,7 +54,8 @@ import { baseURL } from './shared/baseurl';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
